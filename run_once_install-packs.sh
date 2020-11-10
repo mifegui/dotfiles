4a8ada365014aef22326e9562869c596f57a4e2f
mifegui <ericmfguimaraes@tuta.io>
2020-11-10 19:16:50 -0300

    Atlz install-packs

--- run_once_install-packs.sh
+++ run_once_install-packs.sh
@@ -5,4 +5,19 @@ sudo apt install -y \
     newsboat \
     neovim \
     fd-find \
+    fzf \
+    npm \
     ripgrep 
+
+# bat
+apt install -o Dpkg::Options::="--force-overwrite" bat ripgrep
+ln -s /usr/bin/batcat ~/.local/bin/bat
+
+# Zoxide
+curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/ajeetdsouza/zoxide/master/install.sh | sh
+
+# Plug e Plugins vim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+nvim -c PlugInstall
+nvim -c CocInstall
